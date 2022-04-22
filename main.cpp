19b136c5fc70cfce91c890e2195993bb1e530a69
Mike Timmerman <timmermanmike@hotmail.com>
2022-04-22 09:54:28 +0200

    add submodule eigen

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 #include <math.h>
 #include <string>
 
+#include <Eigen/Dense>
+
 using namespace std;
+using namespace Eigen;
 
 int main(int argc, char const *argv[])
 {
 
-    cout << "hello" << endl;
+    MatrixXf A = MatrixXf::Constant(5, 2, 1.5);
+
+    cout << A << endl;
 
 }
