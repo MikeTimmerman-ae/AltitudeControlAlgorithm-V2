9204e2f72e3c0f54ccbee1eda677362998fdcaac
Mike Timmerman <timmermanmike@hotmail.com>
2022-04-22 09:31:38 +0200

    main file and build system

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>                 // #include directives
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    cout << "hello" << endl;
+
+}
